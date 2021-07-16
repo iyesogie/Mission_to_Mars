f4c7c29cea69dd9648610d28504b603572293349
iyesogie <79468835+iyesogie@users.noreply.github.com>
2021-07-15 22:58:31 -0400

    update

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -7,7 +7,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/MarsInfo")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars_app")
 
 
 # Route to render index.html template using data from Mongo
@@ -15,10 +15,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/MarsInfo")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_info = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", marsdata=destination_data)
+    return render_template("index.html", mars=mars_info)
 
 
 # Route that will trigger the scrape function
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
+# In[1]:
+
+
+get_ipython().system('pip install webdriver_manager')
+get_ipython().system('pip install splinter')
+get_ipython().system('pip install bs4')
+
+
+# In[16]:
 
 
 from splinter import Browser
@@ -11,30 +19,35 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# In[19]:
+# In[17]:
 
 
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-# In[20]:
+# In[18]:
 
 
 # Open browser to NASA Mars News Site
 browser.visit('https://mars.nasa.gov/news/')
 
 
-# In[21]:
+# In[19]:
 
 
 html = browser.html
 soup = bs(html, 'html.parser')
 
 # Search for news titles
+title_results = soup.find_all('div', class_='content_title')
+
 # Search for paragraph text under news titles
-news_title = soup.find("div", class_="content_title").get_text() 
-news_p = soup.find("div", class_="article_teaser_body").get_text()
+p_results = soup.find_all('div', class_='article_teaser_body')
+
+# Extract first title and paragraph, and assign to variables
+news_title = title_results[0].text
+news_p = p_results[0].text
 
 print(news_title)
 print(news_p)
@@ -42,7 +55,7 @@ print(news_p)
    
 
 
-# In[22]:
+# In[20]:
 
 
 # Set up Splinter
@@ -50,14 +63,14 @@ executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-# In[23]:
+# In[21]:
 
 
 # Open browser to JPL Featured Image
 browser.visit('https://spaceimages-mars.com')
 
 
-# In[24]:
+# In[22]:
 
 
 # Click through to find full image
@@ -67,8 +80,14 @@ browser.click_link_by_partial_text('FULL IMAGE')
 # In[25]:
 
 
+# Scrape page into Soup
+html = browser.html
+soup = bs(html, 'html.parser')
+
 featured_image_path = soup.find_all('img')[2]["src"]
 featured_image_url = 'https://spaceimages-mars.com'+ featured_image_path
+
+
 print(featured_image_url)
 
 
@@ -78,18 +97,19 @@ print(featured_image_url)
 import pandas as pd
 
 
-# In[27]:
+# In[31]:
 
 
-url = 'https://galaxyfacts-mars.com/'
-tables = pd.read_html(url)
-tables
+# Use Pandas to scrape data
+tables = pd.read_html('https://space-facts.com/mars/')
+
+# Take second table for Mars facts
 df = tables[1]
-MarsFacts_df = df.rename(columns={0: "MarsFacts_name", 1: "MarsFacts_value"}, errors="raise")
-MarsFacts_df.head()
+
+df
 
 
-# In[28]:
+# In[32]:
 
 
 # Convert table to html
@@ -110,7 +130,7 @@ hemisphere_image_urls = [
 hemisphere_image_urls
 
 
-# In[31]:
+# In[32]:
 
 
 FinalDictionary ={
@@ -120,23 +140,5 @@ FinalDictionary ={
     "tables":tables,
     'hemisphere' : hemisphere_image_urls
 }
-FinalDictionary
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
