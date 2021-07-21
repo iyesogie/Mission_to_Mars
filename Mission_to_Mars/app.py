1febaaf97291f6b6f521f6122cdebb5b1886c814
iyesogie <79468835+iyesogie@users.noreply.github.com>
2021-07-21 15:24:09 -0400

    UPDATE

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,41 +1,27 @@
-
-from flask import Flask, render_template, redirect
-from pymongo import MongoClient
+# import necessary libraries
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from . import scrape_mars
-client = MongoClient()
+import scrape_mars
 
-# Create an instance of Flask
+# create instance of Flask app
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
-mars = mongo.mars.find_one()
-# Route to render index.html template using data from Mongo
+# create route that renders index.html template
 @app.route("/")
 def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
-    mars = mongo.mars.find_one()
-    return render_template("Mission_to_Mars/template/index.html", mars=mars)
-
-
-# Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():
-
-
-#@main.route('/')
-#def index():
-    #return redirect(url_for('main.home'))
-
- 
-    mars_data = scrape.scrape_all()
-    scrape.update({}, mars_data, upsert=True)
-    return "Scraping Successful"
-
+def scraper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Complete"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,147 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
 
 
-
+# Dependencies
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
-import pandas as pd
 import time
+import requests
+import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
-
-
-# In[3]:
-
-
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# In[4]:
-
-
-# Open browser to NASA Mars News Site
-browser.visit('https://mars.nasa.gov/news/')
 
 
 # In[6]:
 
 
-html = browser.html
-soup = bs(html, 'html.parser')
-
-# Search for news titles
-title_results = soup.find_all('div', class_='content_title')
-
-# Search for paragraph text under news titles
-p_results = soup.find_all('div', class_='article_teaser_body')
-
-# Extract first title and paragraph, and assign to variables
-news_title = title_results[0].text
-news_p = p_results[0].text
-
-print(news_title)
-print(news_p)
-
-   
+# URL of page to be scraped
+url = 'https://redplanetscience.com'
 
 
 # In[7]:
 
 
-# Set up Splinter
+# Retrieve page with the requests module
+response = requests.get(url)
+
+response
+
+
+# In[8]:
+
+
+# Create BeautifulSoup object; parse with 'html.parser'
+soup = bs(response.text, 'html.parser')
+print(soup)
+
+   
+
+
+# In[9]:
+
+
+news_title = soup.body.p.text
+news_title
+
+
+# In[10]:
+
+
+news_p = soup.body.find_all('p')[1].text
+news_p
+
+
+# In[11]:
+
+
+# Setup splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[12]:
+
+
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
 
 
 # In[13]:
 
 
-# Open browser to JPL Featured Image
-browser.visit('https://spaceimages-mars.com')
+button = browser.find_by_tag('button')[1]
+button.click()
 
 
 # In[14]:
 
 
-# Click through to find full image
-browser.click_link_by_partial_text('FULL IMAGE')
+soup = bs(browser.html, 'html.parser')
+print(soup)
 
 
-# In[25]:
+# In[15]:
 
 
-# Scrape page into Soup
-html = browser.html
-soup = bs(html, 'html.parser')
+featured_image = soup.find('img', class_='fancybox-image').get('src')
 
-featured_image_path = soup.find_all('img')[2]["src"]
-featured_image_url = 'https://spaceimages-mars.com'+ featured_image_path
+featured_image_url = url + '/' + featured_image
+featured_image_url
 
 
-print(featured_image_url)
+# In[18]:
 
 
-# In[26]:
-
-
-import pandas as pd
-
-
-# In[31]:
-
-
-# Use Pandas to scrape data
-tables = pd.read_html('https://space-facts.com/mars/')
-
-# Take second table for Mars facts
-df = tables[1]
-
+url = 'https://galaxyfacts-mars.com/'
+tables = pd.read_html(url)
+tables
+df = tables[0]
 df
 
 
-# In[32]:
+# In[19]:
 
 
-# Convert table to html
-mars_facts_table = [df.to_html(classes='data table table-borderless', index=False, header=False, border=0)]
-mars_facts_table
+html_table = df.to_html()
+html_table
 
 
-# In[29]:
+# In[20]:
 
 
-# Store data in a dictionary
-hemisphere_image_urls = [
-    {"title": "Cerberus Hemisphere", "img_url": "https://marshemispheres.com/images/full.jpg"},
-    {"title": "Schiaparelli Hemisphere", "img_url": "https://marshemispheres.com/images/schiaparelli_enhanced-full.jpg"},
-    {"title": "Syrtis Major Hemisphere", "img_url": "https://marshemispheres.com/images/syrtis_major_enhanced-full.jpg"},
-    {"title": "Valles Marineris Hemisphere", "img_url": "https://marshemispheres.com/images/valles_marineris_enhanced-full.jpg"},
-]
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[21]:
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+hemisphere_image_urls = []
+
+url = 'https://marshemispheres.com/'
+browser.visit(url)
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+hemisphere_names = []
+
+hemisphere_names = soup.find_all('h3')
+hemisphere_names
+
+for x in range(1,4):
+   hemisphere_name = hemisphere_names[x].text
+   
+   browser.links.find_by_partial_text(hemisphere_name).click()
+   
+   html = browser.html
+   
+   soup = bs(html, 'html.parser')
+   
+   pic = soup.find_all('a',{'target':'_blank'})[3]['href']
+   
+   cerberus = {'title': hemisphere_name, 'img_url': pic}
+   
+   hemisphere_image_urls.append(cerberus)
+   
+   browser.links.find_by_partial_text('Back').click()
+
+
+# In[22]:
+
+
 hemisphere_image_urls
-
-
-# In[32]:
-
-
-FinalDictionary ={
-    'news_title':news_title,
-    'news_p': news_p,
-    'featured_image_url' : featured_image_url,
-    "tables":tables,
-    'hemisphere' : hemisphere_image_urls
-}
-FinalDictionary
-
-
-# In[33]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
